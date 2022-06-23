6cc150d59a15cdc392f59be907f43cb8b429f02a
mmarkelov <maks-markel@mail.ru>
2022-06-23 15:38:42 -0400

    Added ability to handle auth response

--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ import typeDefs from './schemas/index.mjs';
 import mocks from './mocks/index.mjs';
 
 const ERROR_ARG = 'err';
+const AUTH_PREFIX = 'auth';
 
 const schema = makeExecutableSchema({ typeDefs });
 
@@ -18,6 +19,24 @@ const resolvers = () => {
   const mockSchemaFields = mockSchema.getQueryType().getFields();
 
   const Query = Object.values(schemaFields).reduce((acc, field) => {
+    const mockResolve = mockSchemaFields[field.name].resolve;
+
+    if (field.name.startsWith(AUTH_PREFIX)) {
+      return {
+        ...acc,
+        [field.name]: (source, args, context, info) => {
+          const token = context.request.headers.get('authorization');
+          if (!token) {
+            throw new GraphQLYogaError('Unauthorized', {
+              code: 'UNAUTHORIZED',
+            });
+          }
+
+          return mockResolve(source, args, context, info);
+        },
+      };
+    }
+
     if (field.args.find((item) => item.name === ERROR_ARG)) {
       return {
         ...acc,
@@ -25,8 +44,6 @@ const resolvers = () => {
           if (args.err) {
             throw new GraphQLYogaError('Error');
           }
-
-          const mockResolve = mockSchemaFields[field.name].resolve;
 
           return mockResolve(source, args, context, info);
         },
